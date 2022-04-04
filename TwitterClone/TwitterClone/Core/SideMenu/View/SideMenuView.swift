d728d9c345d21a8f2795ab21bcfe55929ea43919
Jessica Kim <jessica.kim@outlook.de>
2022-04-04 21:48:46 +0200

    Profile View

---
+++ TwitterClone/TwitterClone/Core/SideMenu/View/SideMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  SideMenuView.swift
+//  TwitterClone
+//
+//  Created by Jessica Kim on 04.04.22.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SideMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuView()
+    }
+}
---
+++ TwitterClone/TwitterClone/Core/SideMenu/ViewModel/SideMenuViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  SideMenuViewModel.swift
+//  TwitterClone
+//
+//  Created by Jessica Kim on 04.04.22.
+//
+
+import Foundation
+
+enum SideMenu
