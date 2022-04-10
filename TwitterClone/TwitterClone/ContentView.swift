99754861c72940eb486fe72d8f79ae8b7a2680af
Jessica Kim <jessica.kim@outlook.de>
2022-04-10 18:53:44 +0900

    Update

--- TwitterClone/TwitterClone/ContentView.swift
+++ TwitterClone/TwitterClone/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world2!")
-            .padding()
+        ZStack(alignment: .topLeading) {
+            MainTabView()
+        }
+        .navigationTitle("Home")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
