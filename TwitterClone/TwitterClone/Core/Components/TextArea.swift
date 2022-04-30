c275acbdde3edecd4d4060b27716921c52468a64
Jessica Kim <jessica.kim@outlook.de>
2022-04-30 22:59:59 +0200

    Update NewTweet

--- TwitterClone/TwitterClone/Core/Components/TextArea.swift
+++ TwitterClone/TwitterClone/Core/Components/TextArea.swift
@@ -14,7 +14,21 @@ struct TextArea: View {
     init(_ placeholder: String, text: Binding<String>) {
         self.placeholder = placeholder
         self._text = text
-        UITextView().appearance().backgroundColor = .clear
+        UITextView.appearance().backgroundColor = .clear
+    }
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            if text.isEmpty {
+                Text(placeholder)
+                    .foregroundColor(Color(.placeholderText))
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 12)
+            }
+            TextEditor(text: $text)
+                .padding(4)
+        }
+        .font(.body)
     }
 }
  
--- TwitterClone/TwitterClone/Core/UploadTweet/Views/NewTweetView.swift
+++ TwitterClone/TwitterClone/Core/UploadTweet/Views/NewTweetView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct NewTweetView: View {
+    @State private var caption = ""
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
         VStack {
             HStack {
                 Button  {
-                    print("Dismiss")
+                    presentationMode.wrappedValue.dismiss()
                 } label: {
                     Text("Cancel")
                         .foregroundColor(Color(.systemBlue))
@@ -37,10 +40,13 @@ struct NewTweetView: View {
             }
             .padding()
             
-            HStack {
+            HStack(alignment: .top) {
                 Circle()
                     .frame(width: 64, height: 64)
+                
+                TextArea("What's happening?", text: $caption)
             }
+            .padding()
         }
     }
 }
